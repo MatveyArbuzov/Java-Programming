0af8ee6528adabfc48d8bd99455089f80bada43e
MatveyArbuzov <103934030+MatveyArbuzov@users.noreply.github.com>
2024-03-12 18:46:22 +0400

    Update DayOfTheWeek.java
    Изменены названия методов
    Исправлен инициализирующий метод (код со считыванием данных перенесён в отдельный метод)
    Обработка исключений разделена на случаи

--- src/main/java/ru/sgu/DayOfTheWeek.java
+++ src/main/java/ru/sgu/DayOfTheWeek.java
@@ -1,5 +1,6 @@
 package main.java.ru.sgu;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class DayOfTheWeek
@@ -13,29 +14,40 @@ public class DayOfTheWeek
     int count;
 
     {
+        this.readingData();
+    }
+
+    void readingData(){
         System.out.println("Введите через пробел день недели и количество дней");
         Scanner scanner = new Scanner(System.in);
         try
         {
             current = Day.valueOf(scanner.next());
             count = scanner.nextInt();
+            if (count < 0)
+                throw new ArithmeticException();
         }
-        catch (Exception e)
+        catch (IllegalArgumentException e)
         {
-            throw new RuntimeException("Введены некорректные данные");
+            throw new RuntimeException("Введён некорректный день недели");
         }
+        catch (InputMismatchException | ArithmeticException e)
+        {
+            throw new RuntimeException("Второй параметр не является корректным");
+        }
+        scanner.close();
     }
 
-    void newDay()
+    Day getANewDay()
     {
         count = (count + current.ordinal()) % 7;
         Day[] days = Day.values();
-        System.out.println(days[count]);
+        return days[count];
     }
 
     public static void main(String[] args)
     {
         DayOfTheWeek object = new DayOfTheWeek();
-        object.newDay();
+        System.out.println(object.getANewDay());
     }
 }
